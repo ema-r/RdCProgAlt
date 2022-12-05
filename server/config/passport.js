1df8820be577e4634c49d00100c8ec1ae781ddbc
MashaUP <andreamasciarelli2@gmail.com>
2022-12-05 11:41:40 +0100

    Passport

---
+++ server/config/passport.js
@@ -0,0 +1,58 @@
+const dotenv = require("dotenv");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const { sendWelcomeMail } = require("../middlewares/mailer");
+
+const User = require("../models/User");
+
+dotenv.config({ path: "./config/.env" });
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID || "",
+      clientSecret: process.env.CLIENT_SECRET || "",
+      callbackURL: process.env.REDIRECT_URI || "",
+      passReqToCallback: true,
+    },
+    async (req, accessToken, refreshToken, profile, done) => {
+      req.session.accessToken = accessToken;
+      req.session.refreshToken = refreshToken;
+      const loggedUser = {
+        googleId: profile.id,
+        givenName: profile.name.givenName,
+        familyName: profile.name.familyName,
+        email: profile.emails[0].value,
+        picture: profile.photos[0].value,
+      };
+      await User.findOne({ googleId: loggedUser.googleId })
+        .then(async (result) => {
+          if (!result) {
+            result = await User.create(loggedUser);
+            sendWelcomeMail(loggedUser.email);
+          }
+          return done(null, result);
+        })
+        .catch((err) => {
+          console.error(err.message);
+        });
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  return done(null, user);
+});
+
+passport.deserializeUser(async (user, done) => {
+  await User.findOne({ googleId: user.googleId })
+    .then((result) => {
+      return done(null, result);
+    })
+    .catch((err) => {
+      console.error(err.message);
+    });
+});
+
+module.exports = passport;
