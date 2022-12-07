46135d88cd039978c897f787f8854f330b7ea8e8
Emanuele <103453667+ema-r@users.noreply.github.com>
2022-12-07 12:43:05 +0100

    Immagine Mongo utilizzata da docker impostata a 4.4.6 per problemi  compatibilita' pc :(
     On branch main
     Your branch is ahead of 'origin/main' by 2 commits.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   docker-compose.yml
    	modified:   nginx/nginx.conf
    	modified:   server/Dockerfile
    	new file:   server/app/server.js
    	new file:   server/server.js

---
+++ server/app/server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function(request, response) {
+	response.writeHead(200, { 'Content-Type': 'text/plain' });
+	response.write('Sono un poliglotta');
+	response.end();
+})
+.listen(8443);
