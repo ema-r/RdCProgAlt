178e9a41420acdc77260c907c28e3df96c53f6eb
MashaUP <andreamasciarelli2@gmail.com>
2022-12-20 11:36:02 +0100

    changes frontend

--- server/index.js
+++ server/index.js
@@ -16,7 +16,14 @@ const { dirname } = require('path');
 const mailer = require('nodemailer');
 const spotifyAuth = require('./middlewares/spotify-auth');
 const {URL} = require('url');
-
+const client_id = process.env.SPOTIFY_CLIENT_ID;
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+const redirect_uri = 'https://localhost:8443/spot/callback';
+const spot_client_id = process.env.SPOTIFY_CLIENT_ID;
+const spot_client_sc = process.env.SPOTIFY_CLIENT_SECRET;
+const spot_redirect_uri = 'https://localhost:8443/spot/callback';
+const app = express();
+const port = new URL(redirect_uri).port;
 const INSTANCE = process.env.INSTANCE || '';
 const MONGO_URI = process.env.MONGO_URI || '';
 const PORT = process.env.PORT || 3001;
@@ -74,10 +81,9 @@ passport.use(
   )
 );
 
-const spot_client_id = process.env.SPOTIFY_CLIENT_ID;
-const spot_client_sc = process.env.SPOTIFY_CLIENT_SECRET;
-const spot_redirect_uri = 'https://localhost:8443/spot/callback';
-const app = express();
+app.use(express.static(path.join(__dirname, 'public')))
+.use(cors())
+.use(spotifyAuth({client_id, client_secret, redirect_uri}));
 app.use(express.static(path.join(__dirname, '/public/css')));
 app.use(express.static(__dirname + 'public'));
 app.use(express.static('public'));
@@ -97,7 +103,6 @@ app.use(express.static('public'));
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 /* get root path */
 app.get('/', (req, res) => {
   res.render('index', { title: 'SongLify' });
@@ -105,35 +110,6 @@ app.get('/', (req, res) => {
 
 /* get API docs */
 app.use('/api-docs', express.static(path.join(__dirname, '/public/docs')));
-
-app.get('/test', (req, res) => {
-  res.render('test', {title: 'test'});
-});
-
-app.post('/test', function(req, res) {
-	var item = req.body.formUrl; //TO DO: INPUT SANITIZATION
-	console.log(item);
-	var slug = item.split('track/').pop();
-	console.log(slug);
-
-	getSpotifyToken(spot_client_token_info, spot_client_auth_options);
-
-	const new_data = '';
-	var api_data_input = {
-		hostname: 'api.spotify.com',
-		port: 443,
-		path: '/v1/tracks/' + slug + '?market=' + 'IT',  //TO DO: IMPLEMENTARE GEOAPI
-		method: 'GET',
-		headers: {
-			'Accept': 'application/json',
-			'Content-type': 'application/json',
-			'Authorization': 'Bearer ' + spot_client_token_info.access_token
-		}
-	};
-
-	callFindSongs(new_data, api_data_input);
-	console.log(new_data);
-});
 
 /* set connection with mongo */
 mongoose
@@ -146,111 +122,9 @@ mongoose
   })
   .catch((err) => {
     console.error(err.message);
-  });
+  }
+);
 
-async function callFindSongs(data, api_data) {
-	return await findSongs(data, api_data);
-}
-
-function findSongs(data, api_req_data) {
-  return new Promise((resolve, reject) => {
-	const request = https.request(api_req_data, (result) => {
-  		console.log('statusCode:', result.statusCode);
-		if (result.statusCode < 200 || response.statusCode > 299) {
-			throw new Error('status code: ' + response.statusCode);
-		}
-		else {
-			result.setEncoding('utf8');
-			let responseBody = '';
-
-     			result.on('data', (d) => {
-				console.log('receiving data')
-				responseBody += d;
-	  	   	});
-			result.on('end', () => {w
-				console.log('end of data')
-				resolve(JSON.parse(responseBody));
-			});
-		}
-  	});
-  	request.on('error', (err) => {
-     		console.log('errore');
-		reject(err);
-  	});
-	request.write(data);
-	request.end();
-  });
-}
-
-function getSpotifyToken(access_token_data, api_req_data) {
-	var current_time = new Date().getTime()/1000;
-	if ((current_time - access_token_data.expires_at) > 3520){
-		var data = ''
-		var newTokenRequest = https.request(api_req_data, (result) => {
-			if (result.statusCode >= 200 || result.statusCode < 300) {
-				result.setEncoding('utf8');
-				let responseBody = '';
-	
-				result.on('data', (d) => {
-					console.log('[SPOTIFY_CLIENT_TOKEN_REFRESH] receiving data');
-					responseBody += d;
-				});
-				result.on('end', () => {
-					console.log('[SPOTIFY_CLIENT_TOKEN_REFRESH] end of data');
-					resolve(JSON.parse(responseBody));
-				});
-			} else {
-				throw new Error('[SPOTIFY_CLIENT_TOKEN_REFRESH] Error: status code')
-			}
-		});
-		newTokenRequest.on('error', (err) => {
-			reject(err);
-		});
-		newTokenRequest.write(data);
-		newTokenRequest.end();
-
-		access_token_data.access_token = data.access_token;
-		access_token_data.expires_at   = current_time+3600;
-	} else {
-		console.log('no need to refresh the client token right now')
-	}
-};
-
-app.get('/spot/token_refresh', function(req, res) {
-	var refresh_token = req.query.refresh_token;
-	var authOptions = {
-		url: 'https://accounts.spotify.com/api/token',
-		method: 'POST',
-		headers: { 'Authorization': 'Basic ' + (Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64')) },
-
-		form: {
-			client_id: process.env.SPOTIFY_CLIENT_ID,
-			client_secret: process.env.SPOTIFY_CLIENT_SECRET,
-			grant_type: 'refresh_token',
-			refresh_token: refresh_token
-		},
-		json: true
-	};
-
-	var req = https.request(authOptions, (res) => {
-		if (res.stausCode < 200 || res.statusCode > 299) {
-			console.log('status code errato su post request refresh token spotify, abortendo ',
-				res.tatusCode);
-			return;
-		};
-		res.setEncoding('utf8');
-		res.on('data', function (chunk) {
-			console.log('BODY: ' + chunk);
-		});
-	});
-	req.on('error', function(e) {
-		console.log('errore refresh token: ' + e.message);
-	});
-
-	req.write('data\n');
-	req.write('data\n');
-	req.end();
-});
 
 app.get('/spot', passport.authenticate('spotify'));
 
@@ -258,31 +132,20 @@ app.get(
   '/spot/callback',
   passport.authenticate('spotify', { failureRedirect: '/#error' }),
   function(req, res) {
-    // Successful authentication, redirect home.
     res.redirect('/spot/info');
   }
 );
 
-  app.get(
+app.get(
 	'/spot/info',
 	passport.authenticate('spotify', {
 	  scope: ['user-read-email', 'user-read-private'],
 	  showDialog: true
 	})
-  );
+);
 
- 
-  const client_id = process.env.SPOTIFY_CLIENT_ID;
-  const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
-  const redirect_uri = 'https://localhost:8443/spot/callback';
 
-  const port = new URL(redirect_uri).port;
-  
-  app.use(express.static(path.join(__dirname, 'public')))
-   .use(cors())
-   .use(spotifyAuth({client_id, client_secret, redirect_uri}));
-  
-  app.listen(port, () => console.log(`Listening on ${port}`));
+app.listen(port, () => console.log(`Listening on ${port}`));
 
 
 console.log('in ascolto su 3000');
