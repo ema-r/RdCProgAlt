f79b56ae143f7e3ab300557c6ea10cc1d93a1a44
Emanuele <103453667+ema-r@users.noreply.github.com>
2022-12-22 17:51:31 +0100

    MongoStore crea connessione a Mongo correttamente, ma utilizza  connessione diretta tra node e DB.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   server/index.js
    	modified:   server/package.json

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const dotenv = require('dotenv');
 const MongoStore = require('connect-mongo');
 const express = require('express');
 const sessions = require('express-session');
-const mongoose = require('mongoose');
 const path = require('path');
 const http = require('http');
 const https = require('https');
@@ -15,23 +14,9 @@ const mailer = require('nodemailer');
 const {URL} = require('url');
 const axios = require('axios');
 const INSTANCE = process.env.INSTANCE || '';
-const MONGO_URI = process.env.MONGO_URI || '';
+const mongoUrl = 'mongodb://mongo:27017'
 const PORT = process.env.PORT || 3001;
 const SPOT_TOKEN = process.env.SPOTIFY_OAUTH_TOKEN;
-
-/* set connection with mongo */
-mongoose
-  .connect(MONGO_URI)
-  .then((result) => {
-    console.log(`${INSTANCE} -> ${result.connection.host}`);
-    app.listen(3001, () => {
-      console.log(`${INSTANCE} -> ${3001}`);
-    });
-  })
-  .catch((err) => {
-    console.error(err.message);
-  }
-);
 
 var generateRandomString = function(length) {
 	var text = '';
@@ -54,11 +39,11 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, '/public/css')));
 app.use(express.static(__dirname + 'public'));
 app.use(express.static('public'));
-
 const oneDay = 1000 * 60 * 60 * 24;
 app.use(sessions({
-	secret: 'keyboardfafgseinvoaubwoubauvhfaibpresjbhswrjnngo',
-  	resave: false,
+	secret: 'keyboard cat',
+	store: MongoStore.create({mongoUrl}),
+	resave: false,
   	saveUninitialized: false,
   	cookie: { secure: true , maxAge: oneDay}
 }));
